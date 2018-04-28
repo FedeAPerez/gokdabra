a48a6d38cd771cdbebb4dfc42cc72aef9015fdcd
fedea <fedeaperez@hotmail.com.ar>
2018-04-28 19:32:01 -0300

    error container

---
+++ src/Container/AppContainer__/MainContainer__/ErrorContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ErrorContainer extends Component {
+    render() {
+        return <article>Error al encontrar el sitio que buscabas</article>;
+    }
+}
+
+export default ErrorContainer;
