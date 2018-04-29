54e13bcd1487d75923cd83c01be594ab33a1d3d8
fedea <fedeaperez@hotmail.com.ar>
2018-04-28 22:40:32 -0300

    estrucutra de estilos

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import AppContainer from './Container/AppContainer';
-import './Styles/BasicStyle.css';
+import './Styles/Basic.css';
+import './Styles/BusinessHeader.css';
+import './Styles/Messages.css';
 import './Styles/Left.css';
 import './Styles/LeftTituloLinea.css';
+import './Styles/Right.css';
 
 ReactDom.render((
     <BrowserRouter>
