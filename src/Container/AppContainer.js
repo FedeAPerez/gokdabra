2d138d33b98bd8936376812d523a6821de377025
fedea <fedeaperez@hotmail.com.ar>
2018-05-01 15:18:12 -0300

    first version of loader

--- src/Container/AppContainer.js
+++ src/Container/AppContainer.js
@@ -12,6 +12,22 @@ import MainContainer from './AppContainer__/MainContainer';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class AppContainer extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleLoad() {
+        setTimeout(function() {
+            var element = document.getElementsByClassName('loader');
+            element[0].style.display = 'none';
+        }.bind(this)
+        , 1000);
+    }
+
+    componentDidMount() {
+        window.addEventListener('load', this.handleLoad);
+    }
+
     render() {
         return(
             <main>
