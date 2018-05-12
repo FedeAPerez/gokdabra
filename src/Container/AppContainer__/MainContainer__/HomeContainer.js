84e7cfa140ae169f090d7af7dc5713f3ea8fca72
fedea <fedeaperez@hotmail.com.ar>
2018-05-11 23:47:12 -0300

    basic index

--- src/Container/AppContainer__/MainContainer__/HomeContainer.js
+++ src/Container/AppContainer__/MainContainer__/HomeContainer.js
@@ -9,7 +9,8 @@ class HomeContainer extends Component {
         return (
             <section className="home">
                 <h1 className="home-title">KDABRA</h1>
-                <h1 className="home-tagline">Parece que estas buscando en el lugar indicado</h1>
+                <h2 className="home-tagline">¿Quéres comunicarte con tus clientes y fans como lo harían con WhatsApp y Messenger?</h2>
+                <h3 className="home-tagline-contact">Envia un mail a hola@gokdabra.com</h3>
             </section>
         );
     }
