0971156f9cb3f4ed82fde67bf9ee85658444504e
fedea <fedeaperez@hotmail.com.ar>
2018-05-13 15:12:48 -0300

    comunicación

--- src/Container/AppContainer__/MainContainer__/HomeView.js
+++ src/Container/AppContainer__/MainContainer__/HomeView.js
@@ -8,6 +8,7 @@ import FooterContainer from './HomeViewFolder__/FooterContainer';
 class HomeView extends Component {
     render() {
         return (
+            <main>
             <section className="home">
             	<div className="container">
 					<img className="wave-sgv" src={'content/images/wave.svg'} />
@@ -16,8 +17,16 @@ class HomeView extends Component {
                 <h1 className="home-title">KDABRA</h1>
                 <h2 className="home-tagline">¿Querés comunicarte con tus clientes como lo hacen <br />Coca-Cola y Johnnie Walker?</h2>
                 </article>
-                <FooterContainer />
+                <hr />
+                <article className="home-tag">
+                    <h2>Estamos alineados con tu negocio y con lo que querés para tus clientes.</h2>
+                    <h3>Creemos que la comunicación y la conversación son vitales para las ventas de tu negocio</h3>
+                </article>
+                
             </section>
+            
+            <FooterContainer />
+            </main>
         );
     }
 }
