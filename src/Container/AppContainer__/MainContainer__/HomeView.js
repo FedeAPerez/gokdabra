8b2673a7557526cda87d0abebd0c3c0c593feb71
fedea <fedeaperez@hotmail.com.ar>
2018-05-13 15:15:28 -0300

    comunicación

--- src/Container/AppContainer__/MainContainer__/HomeView.js
+++ src/Container/AppContainer__/MainContainer__/HomeView.js
@@ -19,8 +19,13 @@ class HomeView extends Component {
                 </article>
                 <hr />
                 <article className="home-tag">
+                    <h2>Las personas se comunican con mensajes.</h2>
+                    <h3>En el mundo se envían 60 mil millones de mensajes entre WhatsApp y Messenger.</h3>
+                </article>
+                <hr />
+                <article className="home-tag">
                     <h2>Estamos alineados con tu negocio y con lo que querés para tus clientes.</h2>
-                    <h3>Creemos que la comunicación y la conversación son vitales para las ventas de tu negocio</h3>
+                    <h3>Creemos que la comunicación y la conversación son vitales para distinguirte como una marca relevante.</h3>
                 </article>
                 
             </section>
