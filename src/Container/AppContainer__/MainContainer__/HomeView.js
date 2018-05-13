1487eea6b1ae30fb79c50ed743b8bca282f80f33
fedea <fedeaperez@hotmail.com.ar>
2018-05-13 14:39:09 -0300

    home content

--- src/Container/AppContainer__/MainContainer__/HomeView.js
+++ src/Container/AppContainer__/MainContainer__/HomeView.js
@@ -12,8 +12,10 @@ class HomeView extends Component {
             	<div className="container">
 					<img className="wave-sgv" src={'content/images/wave.svg'} />
 				</div>
+                <article className="home-content">
                 <h1 className="home-title">KDABRA</h1>
                 <h2 className="home-tagline">¿Querés comunicarte con tus clientes como lo hacen <br />Coca-Cola y Johnnie Walker?</h2>
+                </article>
                 <FooterContainer />
             </section>
         );
