08a99b989594c8d7637cf39df41fd59fe86ec529
fedea <fedeaperez@hotmail.com.ar>
2018-05-26 19:11:54 -0300

    messages changing

--- src/RouteAdmin/SettingsContainer.js
+++ src/RouteAdmin/SettingsContainer.js
@@ -3,11 +3,11 @@
  * Código de librerías externas
  * */
 import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
 /* *
  * Código de librerías internas
  * */
 import  { OptionsAPI }  from '@gokdabra/gokdabraapi';
-
 import  { OptionsBusinessAPI }  from '@gokdabra/gokdabraapi';
 /* *
  * Hojas de Estilo y Constantes
@@ -20,7 +20,11 @@ class SettingsContainer extends Component {
         this.state = {
             options_list: [],
             profile_completed: 0,
-            percentage_completed: '0%'
+            percentage_completed: '0%',
+            setting_item : false,
+            setting_option : "",
+            setting_show_message : "",
+            setting_value : ""
         };
     }
 
@@ -31,7 +35,7 @@ class SettingsContainer extends Component {
         .then((res) => {
             res.data.options_list.forEach(element => {
                 var indexElement = this.state.options_list.findIndex(p => p.id_option == element.id_option);
-                if(indexElement > 0) {
+                if(indexElement >= 0) {
                     this.state.options_list[indexElement].description = element.show_message;
                     this.state.profile_completed += 1;
                 }
@@ -40,14 +44,13 @@ class SettingsContainer extends Component {
             this.profileCompletedPercentage();
         })
         .catch((err) => {
+
         });
     }
 
     profileCompletedPercentage() {
         if(this.state.options_list) {
-            console.log(this.state.profile_completed);
             var perce = (this.state.profile_completed / this.state.options_list.length) *100;
-            console.log(perce);
             this.state.percentage_completed = "%"+perce;
             this.setState(this.state);
         }
@@ -58,11 +61,9 @@ class SettingsContainer extends Component {
         .then((res) => {
             this.state.options_list = res.data.options_list;
             this.setState(this.state);
-
             this.fillOptionsBusiness();
         })
         .catch((err) => {
-            console.error(err);
         });
     }
 
@@ -70,11 +71,60 @@ class SettingsContainer extends Component {
         this.fillOptions();
     }
 
-    modifyElement() {
-
+    modifyElement(id_option, show_message) {
+        this.state.setting_item = true;
+        this.state.setting_option = id_option;
+        this.state.setting_show_message = show_message;
+        this.setState(this.state);
     }
 
+    modifyElementClose() {
+        this.state.setting_item = false;
+        this.state.setting_option = "";
+        this.state.setting_show_message = "";
+        this.state.setting_value = "";
+        this.setState(this.state);
+    }
+
+    modifyElementSave() {
+        var obj = {};
+        obj.id_option = this.state.setting_option;
+        obj.id_business = this.props.businessObject.business_name.toLowerCase();
+        obj.show_message = this.state.setting_value;
+        OptionsBusinessAPI.saveOptionForBusiness(obj)
+        .then((res) => {
+            console.log(res);
+            this.modifyElementClose();
+            this.fillOptionsBusiness();
+        })
+        .catch((err) => {
+
+        });
+    }
+
+    handleChange = (e) => {        
+        e.preventDefault();
+        this.setState(
+            {
+                setting_value: e.target.value
+            }
+        );
+	};
+
     render() {
+        const inputStyle = {
+            margin: '0em auto',
+            marginLeft: '1em',
+            marginRight: '1em',
+            fullWidth: true,
+            display: 'inline-block',
+            backgroundColor: 'white',
+            borderRadius: '1em'
+        }
+        const hintStyle = {
+            color: '#95a5a6'
+        }
+
         return (
             <main className="admin-settings-container">
                 <section className="admin-settings">
@@ -92,7 +142,11 @@ class SettingsContainer extends Component {
                                     <p className="admin-settings-item-description" key={"admin-settings-item-description-"+index}>
                                         {element.description != undefined ? element.description : "¡Configurá este mensaje para que tus clientes lo vean en KDABRA!"}
                                     </p>
-                                    <button className="admin-settings-modify-button" key={"admin-settings-modify-button-"+index}>
+                                    <button 
+                                        onClick = { () => this.modifyElement(element.id_option, element.show_message) }
+                                        className="admin-settings-modify-button" 
+                                        key={"admin-settings-modify-button-"+index}
+                                    >
                                         { element.description != undefined ? "Modificar" : "Agregar" }
                                     </button>
                                 </div>
@@ -101,6 +155,40 @@ class SettingsContainer extends Component {
                         })
                     }
                 </section>
+                {
+                    this.state.setting_item &&    
+                    <section className="admin-settings-modify">
+                        <section className="admin-settings-modify-area">
+                            <img 
+                                className="admin-settings-modify-close"
+                                src={'/content/images/cancel.svg'}
+                                onClick={this.modifyElementClose.bind(this)}
+                            />
+                            <h2>{
+                                this.state.setting_show_message
+                            }</h2>
+
+                            <h3>
+                                Este mensaje va a ayudar a tus clientes a entender mejor tu negocio y obtener toda la información de antemano.
+                            </h3>
+
+                            <TextField 
+                                hintStyle= { hintStyle }
+                                multiLine={true}
+                                style= { inputStyle }
+                                onChange={ this.handleChange }
+                                value={ this.state.setting_value }
+                                hintText={"Escribí tu mensaje..."}
+                            />
+                            <button 
+                                onClick={this.modifyElementSave.bind(this)}
+                                className="admin-settings-modify-save" 
+                            >
+                                ¡Guardar cambios!
+                            </button>
+                        </section>
+                    </section>
+                }
             </main>
         );
     }
