98ac5171c82f55d644b5dc65856d89ed80398621
fedea <fedeaperez@hotmail.com.ar>
2018-05-28 23:21:45 -0300

    test de icons on onboarding

--- src/RouteOnboarding/OnboardingView.js
+++ src/RouteOnboarding/OnboardingView.js
@@ -39,14 +39,23 @@ class OnboardingView extends Component {
                 >
                     <div className="onboarding-box onboarding-home">
                         <h2>El futuro y presente de la comunicación está en los mensajes...</h2>
+                        <div className="onboarding-image-container">
+                        <img className="onboarding-image" src={"/content/images/icons/1.png"} alt="mensajes"/>
+                        </div>
                         <h3>En el mundo se envían 60 mil millones de mensajes entre WhatsApp y Messenger.</h3>
                     </div>
                     <div className="onboarding-box onboarding-hook">
                         <h2>Alineados con tu negocio y con lo que está ocurriendo en el mundo.</h2>
+                        <div className="onboarding-image-container">
+                        <img className="onboarding-image" src={"/content/images/icons/2.png"} alt="mensajes"/>
+                        </div>
                         <h3>Johnnie Walker y Coca-Cola son dos de las grandes marcas que utilizaron mensajes para potenciarse.</h3>
                     </div>
                     <div className="onboarding-box onboarding-product">
                         <h2>¡Queremos construir esta experiencia con nuestros usuarios!</h2>
+                        <div className="onboarding-image-container">
+                        <img className="onboarding-image" src={"/content/images/icons/3.png"} alt="mensajes"/>
+                        </div>
                         <h3>Sumate antes del lanzamiento y ayudanos a definir el futuro de <span className="kdabra-orange-text">KDABRA</span>.</h3>
                     </div>
                 </SwipeableViews>
