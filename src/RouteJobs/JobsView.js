67306fc5378c8c07822d52af19ce438543c69453
fedea <fedeaperez@hotmail.com.ar>
2018-09-30 13:05:30 -0300

    new exp jobs

--- src/RouteJobs/JobsView.js
+++ src/RouteJobs/JobsView.js
@@ -21,35 +21,44 @@ class JobsView extends Component {
                         Mis Aplicaciones - Federico Pérez
                     </h2>
                 </div>
-                <div className="text-chat-arq">
+
+                <article className="article">
+                    <h3 className="new-exp-text-aclara">
+                        Jorgelina Gómez <span>Experta en RR.HH</span>
+                    </h3>
+                    <div className="text-chat-arq">
                     <h2 className="new-exp-text">
                         Fede, antes que nada me gustó tu aplicación.
                         Pero me gustaría que hagamos incapíe sobre las aclaraciones que hace nuestro líder técnico.
                         ¡Buen trabajo!
                     </h2>
-                    <h3 className="new-exp-text-aclara">
-                        Jorgelina Gómez <span>Experta en RR.HH.</span>
-                    </h3>
-                </div>
 
-                <div className="text-chat-arq">
+                </div>
+                </article>
+
+                <article className="article">
+                    <h3 className="new-exp-text-aclara">
+                        José Gómez <span>Arquitecto en sistemas</span>
+                    </h3>
+                    <div className="text-chat-arq">
                     <h2 className="new-exp-text">
                         ¡Buenas tardes! Me gustaría más detalle acerca de tus conocimientos en ReactJS y Redux.
                         ¿Podrías comentarnos un poco de tu experiencia?
                     </h2>
-                    <h3 className="new-exp-text-aclara">
-                        José Gómez <span>Arquitecto en sistemas.</span>
-                    </h3>
-                </div>
 
-                <div className="text-chat-arq user">
-                    <h2 className="new-exp-text">
-                        Sí, les agrego esas aclaraciones en el documento.<span className="emoji">&#x1F4C2;</span>
-                    </h2>
-                    <h3 className="new-exp-text-aclara">
-                        Federico Pérez
-                    </h3>
                 </div>
+                </article>
+
+                <article className="article-user">
+                    <h3 className="new-exp-text-aclara">
+                            Federico Pérez
+                    </h3>
+                    <div className="text-chat-arq user">
+                        <h2 className="new-exp-text">
+                            Sí, les agrego esas aclaraciones en el documento.<span className="actionable">Ver documento</span>
+                        </h2>
+                    </div>
+                </article>
 
 
             </div>
